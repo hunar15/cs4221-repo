094b0494b6f7b20bb676338f9e952aaa29fe1ab6
hunar <hunur.khanna@gmail.com>
2013-10-13 16:45:00 +0800

    Created OP, ISA relationship objects in the canvas

--- js/jsPlumbSetup.js
+++ js/jsPlumbSetup.js
@@ -1,3 +1,5 @@
+
+
 function getInnerTextHTMLContent(value) {
 	
 	return "<div class=\"inner-text\">" +
@@ -41,11 +43,11 @@ function createAttributeWithName(name) {
 	return newEntity;
 }
 
-function createRelationshipWithName (name) {
+function createRelationshipWithName (name, id) {
 	/*
 	Name is the unique identifier of the relationship
 	*/
-    var newEntity = "<div id=\""+name+"-relationship\" class=\"relationship\"";
+    var newEntity = "<div id=\""+id+"\" class=\"relationship\"";
     
     if(name.length >= 10 && name.length < 20)
     {
@@ -69,6 +71,48 @@ function createRelationshipWithName (name) {
 	return newEntity;
 }
 
+function createISARelationshipWithId (id) {
+	/*
+	Id is the unique identifier of the relationship
+	*/
+    var newEntity = "<div id=\""+id+"\" class=\"isa-relationship\">"+getInnerTextHTMLContent("IS-A")+"</div>";
+
+	return newEntity;
+}
+
+function createOPRelationshipWithIdAndType (id, type) {
+	/*
+	Id is the unique identifier of the relationship
+	Type is from {"Union","Intersection"}
+	*/
+	type = type.charAt(0).toUpperCase() + type.slice(1);
+    var newEntity = "<div id=\""+id+"\" class=\"op-relationship\">" + getInnerTextHTMLContent(type) +"</div>";
+
+	return newEntity;
+}
+
+function createDRelationshipWithName (name, id) {
+	/*
+	Id is the unique identifier of the relationship
+	Type is from {"Union","Intersection"}
+	*/
+	name = name.charAt(0).toUpperCase() + name.slice(1);
+    var newEntity = "<div id=\""+id+"\" class=\"dc-relationship\">" + getInnerTextHTMLContent("By " + name) +"</div>";
+
+	return newEntity;
+}
+
+function createEXRelationshipWithName (name, id) {
+	/*
+	Id is the unique identifier of the relationship
+	Type is from {"Union","Intersection"}
+	*/
+	name = name.charAt(0).toUpperCase() + name.slice(1);
+    var newEntity = "<div id=\""+id+"\" class=\"ex-relationship\">" + getInnerTextHTMLContent("EX <br/>Has " + name) +"</div>";
+
+	return newEntity;
+}
+
 function isEntityType (element) {
 	if(element.hasClass('entity') || element.hasClass('weak-entity')) {
 		return true;
@@ -78,9 +122,9 @@ function isEntityType (element) {
 }
 
 function isRelationshipType( element ) {
-	if(element.hasClass('relationship') || element.hasClass('isa-relationship' ||
+	if(element.hasClass('relationship') || element.hasClass('isa-relationship') || 
 		element.hasClass('op-relationship') || element.hasClass('dc-relationship') ||
-		element.hasClass('ex-relationship') || element.hasClass('id-relationship'))) {
+		element.hasClass('ex-relationship') || element.hasClass('id-relationship')) {
 		return true;
 	} else {
 		return false;
